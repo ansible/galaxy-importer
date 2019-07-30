b98dea160620c6f56abcb4ce4d6b682616c2dbd5
Andrew Crosby <acrosby@redhat.com>
2019-07-30 15:28:46 -0400

    Write cli output to file
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- galaxy_importer/main.py
+++ galaxy_importer/main.py
@@ -16,37 +16,62 @@
 # along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
 
 import argparse
+import json
 import logging
+from pprint import pprint
 import sys
 
 from . import collection
 
 
-def start_import(filepath):
-    """Outputs result of galaxy_importer import process.
+def main(args=None):
+    logging.basicConfig(
+        stream=sys.stdout,
+        format='%(levelname)s: %(message)s',
+        level=logging.INFO)
+    args = parse_args(args)
 
-    :param filepath: Artifact file to import.
-    """
-    json_data = collection.import_collection(filepath)
-    # TODO: make default output go to file
-    print(json_data)
+    json_data = call_importer(filepath=args.filepath)
+
+    if args.print_result:
+        pprint(json.loads(json_data))
+
+    write_output_file(json_data)
 
 
 def parse_args(args):
     parser = argparse.ArgumentParser(
         description='Import collection and run linting')
     parser.add_argument(
-        '-f', '--filepath', default=None, help='Artifact to import.')
+        '-f', '--filepath',
+        default=None,
+        required=True,
+        help='Artifact to import.')
+    parser.add_argument(
+        '--print_result',
+        dest='print_result',
+        action='store_true',
+        help='Print importer result.')
     return parser.parse_args(args=args)
 
 
-def main(args=None):
-    logging.basicConfig(
-        stream=sys.stdout,
-        format='%(message)s',
-        level=logging.INFO)
-    args = parse_args(args)
-    start_import(filepath=args.filepath)
+def call_importer(filepath):
+    """Returns result of galaxy_importer import process.
+
+    :param filepath: Artifact file to import.
+    """
+    json_data = collection.import_collection(filepath)
+    data = json.loads(json_data)
+    if data['result'] == 'completed':
+        print('Importer processing completed successfully')
+    else:
+        print(f'Error during importer proccessing: {data["error"]}')
+    return json_data
+
+
+def write_output_file(json_data):
+    with open('importer_result.json', 'w') as output_file:
+        output_file.write(json_data)
 
 
 if __name__ == '__main__':
