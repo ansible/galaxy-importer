d44d58315e5190229b4441aef2ea83657d606bc4
Andrew Crosby <acrosby@redhat.com>
2019-09-11 13:30:54 -0400

    Run ansible-lint on roles (#23)

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -17,7 +17,11 @@
 
 import attr
 import json
+import os
 import pytest
+import re
+import shutil
+import tempfile
 from unittest import mock
 
 from galaxy_importer import constants
@@ -59,6 +63,14 @@ def loader_module():
         root='/tmp/tmpiskt5e2n')
 
 
+@pytest.fixture
+def loader_role():
+    return loaders.RoleLoader(
+        content_type=constants.ContentType.ROLE,
+        rel_path='roles/my_sample_role',
+        root=None)
+
+
 def test_get_loader_cls():
     res = loaders.get_loader_cls(constants.ContentType.MODULE)
     assert issubclass(res, loaders.PluginLoader)
@@ -73,12 +85,8 @@ def test_init_plugin_loader(loader_module):
     assert loader_module.name == 'my_sample_module'
 
 
-def test_init_role_loader():
-    loader = loaders.RoleLoader(
-        content_type=constants.ContentType.ROLE,
-        rel_path='roles/my_sample_role',
-        root='/tmp/tmpiskt5e2n')
-    assert loader.name == 'my_sample_role'
+def test_init_role_loader(loader_role):
+    assert loader_role.name == 'my_sample_role'
 
 
 def test_bad_plugin_name():
@@ -200,3 +208,66 @@ def test_ansible_doc_missing_key(mocked_run_ansible_doc, loader_module):
     mocked_run_ansible_doc.return_value = ansible_doc_output
     loader_module._get_doc_strings()
     assert loader_module.doc_strings is None
+
+
+ANSIBLELINT_TASK_OK = """---
+- name: Add mongodb repo apt_key
+  become: true
+  apt_key: keyserver=hkp
+  until: result.rc == 0
+"""
+
+ANSIBLELINT_TASK_SUDO_WARN = """---
+- name: edit vimrc
+  sudo: true
+  lineinfile:
+    path: /etc/vimrc
+    line: '# added via ansible'
+"""
+
+
+@pytest.fixture
+def temp_root():
+    try:
+        tmp = tempfile.mkdtemp()
+        yield tmp
+    finally:
+        shutil.rmtree(tmp)
+
+
+def test_ansiblelint_file(loader_role):
+    with tempfile.NamedTemporaryFile('w') as fp:
+        fp.write(ANSIBLELINT_TASK_SUDO_WARN)
+        fp.flush()
+        res = list(loader_role._lint_role(fp.name))
+    assert 'deprecated sudo' in ' '.join(res).lower()
+
+
+def test_ansiblelint_role(temp_root, loader_role):
+    task_dir = os.path.join(temp_root, 'tasks')
+    os.makedirs(task_dir)
+    with open(os.path.join(task_dir, 'main.yml'), 'w') as fp:
+        fp.write(ANSIBLELINT_TASK_SUDO_WARN)
+        fp.flush()
+        res = list(loader_role._lint_role(temp_root))
+    assert 'deprecated sudo' in ' '.join(res).lower()
+    lint_output_path = re.compile(r'^tmp.+\/tasks\/main\.yml.+$')
+    assert re.match(lint_output_path, res[0])
+
+
+def test_ansiblelint_role_no_warn(temp_root, loader_role):
+    task_dir = os.path.join(temp_root, 'tasks')
+    os.makedirs(task_dir)
+    with open(os.path.join(task_dir, 'main.yml'), 'w') as fp:
+        fp.write(ANSIBLELINT_TASK_OK)
+        fp.flush()
+        res = list(loader_role._lint_role(temp_root))
+    assert res == []
+
+
+@mock.patch('galaxy_importer.loaders.Popen')
+def test_ansible_lint_exception(mocked_popen, loader_role):
+    mocked_popen.return_value.stdout = ''
+    mocked_popen.return_value.wait.return_value = 1
+    res = list(loader_role._lint_role('.'))
+    assert 'Exception running ansible-lint' in res[0]
