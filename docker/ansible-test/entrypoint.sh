6ad60e6c3aa5ff2c6f86414d4e5c82e5eabf448a
Andrew Crosby <acrosby@redhat.com>
2020-03-04 15:58:56 -0500

    Update entrypoint script
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- docker/ansible-test/entrypoint.sh
+++ docker/ansible-test/entrypoint.sh
@@ -1,22 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Extract collection to path needed for ansible-test sanity
 mkdir -p /ansible_collections/placeholder_namespace/placeholder_name
-cd /ansible_collections/placeholder_namespace/placeholder_name
+pushd ansible_collections/ > /dev/null
+pushd placeholder_namespace/placeholder_name/ > /dev/null
 
 echo "Copying and extracting collection archive..."
 cp /archive/archive.tar.gz .
 tar -xzf archive.tar.gz
 
+# Get variables from collection metadata
+read NAMESPACE NAME VERSION < <(python3 <<EOF
+import json
+with open('MANIFEST.json') as fp:
+    metadata = json.load(fp)['collection_info']
+values = metadata['namespace'], metadata['name'], metadata['version']
+print(*values)
+EOF
+)
+
 # Rename placeholders in path
-NAMESPACE=$(python3 -c "import json; f = open('MANIFEST.json'); namespace = json.load(f)['collection_info']['namespace']; print(namespace); f.close")
-NAME=$(python3 -c "import json; f = open('MANIFEST.json'); name = json.load(f)['collection_info']['name']; print(name); f.close")
-VERSION=$(python3 -c "import json; f = open('MANIFEST.json'); version = json.load(f)['collection_info']['version']; print(version); f.close")
-cd ../../
-mv placeholder_namespace/placeholder_name placeholder_namespace/$NAME
-mv placeholder_namespace/ $NAMESPACE
-cd /ansible_collections/$NAMESPACE/$NAME
+popd > /dev/null
+mv placeholder_namespace/placeholder_name placeholder_namespace/"$NAME"
+mv placeholder_namespace/ "$NAMESPACE"
+cd /ansible_collections/"$NAMESPACE"/"$NAME"
 
 echo "Using $(ansible --version | head -n 1)"
 
