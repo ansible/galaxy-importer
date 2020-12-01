590910c98e05adf78beb71e522585e390d56f72d
Yanis Guenane <yguenane@redhat.com>
2020-12-01 16:43:28 +0100

    setup.py: Allow greater version flexibility (#104)
    This commit allows one to specify an alternative name for the build.
    
    The goal of this commit is to allow nightly builds in the form of
    
    ALTERNATE_VERSION = 0.2.1dev20201130gitb48b56e python setup.py sdist
    
    Issue: AAH-185
    Signed-off-by: Yanis Guenane <yguenane@redhat.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import os
 import setuptools
 
 from galaxy_importer import __version__
 
+version = os.environ.get("ALTERNATE_VERSION", __version__)
+
 setuptools.setup(
-    version=__version__,
+    version=version,
 )
