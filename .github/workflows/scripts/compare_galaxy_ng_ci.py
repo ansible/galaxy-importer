f68fc7241b601d69af0455557beb1ad20c4ec60d
Andrew Crosby <acrosby@redhat.com>
2021-07-09 14:24:29 -0400

    Update check_commits.py script to match galaxy_ng (#127)
    No-Issue

---
+++ .github/workflows/scripts/compare_galaxy_ng_ci.py
@@ -0,0 +1,22 @@
+import sys
+
+import requests
+
+
+def main():
+    galaxy_ng_url = (
+        "https://raw.githubusercontent.com/ansible/galaxy_ng/master/"
+        ".github/workflows/scripts/check_commits.py")
+
+    with open(".github/workflows/scripts/check_commits.py") as f:
+        galaxy_importer_text = f.read()
+    galaxy_ng_text = requests.get(galaxy_ng_url).text
+
+    if galaxy_importer_text == galaxy_ng_text:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
