8206b338327e040b8bcdb5403f658a40a24e7987
James Tanner <tanner.jc@gmail.com>
2021-10-19 08:33:31 -0400

    andrew patch 2021-10-09

--- galaxy_importer/collection.py
+++ galaxy_importer/collection.py
@@ -35,20 +35,96 @@ default_logger = logging.getLogger(__name__)
 CollectionFilename = namedtuple("CollectionFilename", ["namespace", "name", "version"])
 
 
-def import_collection(file, filename=None, file_url=None, logger=None, cfg=None):
+def import_collection(
+    file=None,
+    filename=None,
+    file_url=None,
+    git_clone_path=None,
+    output_path=None,
+    logger=None,
+    cfg=None,
+):
     """Process import on collection artifact file object.
 
+    :param file: file handle of collection artifact.
+    :param filename: namedtuple of CollectionFilename.
+    :param file_url: storage url of collection artifact.
+    :param git_clone_path: path to git repo directory of collection pre artifact build.
+    :param output_path: path where collection build tarball file will be written.
+    :param logger: Optional logger instance.
+    :param cfg: Optional config.
+
     :raises exc.ImporterError: On errors that fail the import process.
+
+    :return: metadata if `file`  provided, (metadata, filepath) if `git_clone_path` provided
     """
+
     logger.info(f"Importing with galaxy-importer {__version__}")
     if not cfg:
         config_data = config.ConfigFile.load()
         cfg = config.Config(config_data=config_data)
     logger = logger or default_logger
+
+    if (file and git_clone_path) or not (file or git_clone_path):
+        raise exc.ImporterError("Expected either 'file' or 'git_clone_path' to be populated")
+
+    if git_clone_path:
+        filepath = _build_collection(git_clone_path, output_path, logger)
+        with open(filepath, "rb") as fh:
+            metadata = _import_collection(fh, filename=None, file_url=None, logger=logger, cfg=cfg)
+        return (metadata, filepath)
+
     return _import_collection(file, filename, file_url, logger, cfg)
 
 
+def sync_collection(git_clone_path, output_path, logger=None, cfg=None):
+    """Process collection metadata without linting to support pulp-ansible sync.
+
+    Call _import_collection() with an overridden config to
+    process metadata without linting and without running ansible-test.
+    """
+
+    logger = logger or default_logger
+    if not cfg:
+        config_data = config.ConfigFile.load()
+        cfg = config.Config(config_data=config_data)
+
+    cfg.run_ansible_test = False
+    cfg.run_ansible_lint = False
+    cfg.run_flake8 = False
+
+    filepath = _build_collection(git_clone_path, output_path, logger)
+    with open(filepath, "rb") as fh:
+        metadata = _import_collection(fh, filename=None, file_url=None, logger=logger, cfg=cfg)
+    return (metadata, filepath)
+
+
+def _build_collection(git_clone_path, output_path, logger=None):
+    """Runs `ansible-galaxy collection build` and returns artifact filepath."""
+
+    logger = logger or default_logger
+    logger.info("Building collection tarball with ansible-galaxy collection build")
+
+    cmd = ["ansible-galaxy", "collection", "build", "--output-path", output_path]
+    result = subprocess.run(cmd, cwd=git_clone_path, capture_output=True)
+
+    if result.returncode != 0:
+        raise exc.ImporterError(
+            "Error running `ansible-galaxy collection build`: {}".format(
+                result.stderr.decode("utf-8").rstrip()
+            )
+        )
+
+    # TODO: use regex to get filename from stdout, compine with output_path in case cli output
+    # ever changes from: Created collection for <namespace>.<name> at /<path>/<artifact>.tar.gz
+    stdout = result.stdout.decode("utf-8").rstrip()
+    filepath = stdout.split(" ")[-1]
+    return filepath
+
+
 def _import_collection(file, filename, file_url, logger, cfg):
+    """Returns collection version metadata."""
+
     with tempfile.TemporaryDirectory(dir=cfg.tmp_root_dir) as tmp_dir:
         sub_path = "ansible_collections/placeholder_namespace/placeholder_name"
         extract_dir = os.path.join(tmp_dir, sub_path)
