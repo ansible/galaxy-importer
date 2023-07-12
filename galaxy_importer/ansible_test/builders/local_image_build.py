112723ab3e873f2d96754bf9aff92081b1454f83
James Tanner <tanner.jc@gmail.com>
2023-07-12 10:00:39 -0400

    Switch to centos based dockerfile.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_importer/ansible_test/builders/local_image_build.py
+++ galaxy_importer/ansible_test/builders/local_image_build.py
@@ -15,10 +15,12 @@
 # You should have received a copy of the Apache License
 # along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
 
+import glob
 import logging
 import os
 import pkg_resources
 import shutil
+import subprocess
 import tempfile
 
 from galaxy_importer import exceptions
@@ -91,17 +93,24 @@ class Build(object):
             f.writelines(lines)
 
     def _build_image_with_artifact(container_engine, dir):
-        pkg_entrypoint = pkg_resources.resource_filename(
-            "galaxy_importer", "ansible_test/container/entrypoint.sh"
+        dockerfile = pkg_resources.resource_filename(
+            "galaxy_importer", "ansible_test/container/Dockerfile"
         )
-        eda_linting = pkg_resources.resource_filename(
-            "galaxy_importer", "ansible_test/container/eda/tox.ini"
-        )
-        shutil.copyfile(pkg_entrypoint, os.path.join(dir, "entrypoint.sh"))
-        os.mkdir(os.path.join(dir, "eda"))
-        shutil.copyfile(eda_linting, os.path.join(dir, "eda", "tox.ini"))
+        src = os.path.dirname(dockerfile)
+        paths = glob.glob(f"{src}/*")
+        for path in paths:
+            if os.path.basename(path) == 'Dockerfile':
+                continue
+            dst = os.path.join(dir, os.path.basename(path))
+            print(f"{path} > {dst}")
+            if os.path.isdir(path):
+                shutil.copytree(path, dst)
+            else:
+                shutil.copyfile(path, dst)
 
-        cmd = [container_engine, "build", ".", "--quiet"]
+        #cmd = [container_engine, "build", ".", "--quiet"]
+        cmd = [container_engine, "build", "."]
+
         proc = Popen(
             cmd,
             cwd=dir,
@@ -111,13 +120,22 @@ class Build(object):
         )
 
         image_id = ""
+        build_log = []
         for line in proc.stdout:
-            image_id = line.strip()
+            line = line.rstrip()
+            if not line:
+                continue
+            print(line)
+            build_log.append(line)
+            if 'sha256:' in line and 'FROM' not in line:
+                words = line.split()
+                words = [x for x in words if x.startswith('sha256:')]
+                image_id = words[0]
 
         return_code = proc.wait()
         if return_code != 0:
             raise exceptions.AnsibleTestError(
-                "An exception occurred in {}, returncode={}".format(" ".join(cmd), return_code)
+                "An exception occurred in {}, returncode={} {}".format(" ".join(cmd), return_code, "\n".join(build_log))
             )
         if container_engine == "docker":
             image_id = image_id.split(":")[-1]
