e31221e6f5f9fa95cc11a2ab271580edbb15c427
jerabekjiri <jerabekjirka@email.cz>
2023-11-02 17:37:27 +0100

    fix test
    Issue: AAH-2823

--- tests/unit/test_schema_legacy.py
+++ tests/unit/test_schema_legacy.py
@@ -228,7 +228,7 @@ def test_invalid_dependency_dict_type(galaxy_info, invalid_dict):
 )
 def test_valid_dependency_types(galaxy_info, dependencies):
     legacy_metadata = LegacyMetadata(LegacyGalaxyInfo(**galaxy_info), dependencies)
-    assert isinstance(legacy_metadata, list)
+    assert isinstance(legacy_metadata.dependencies, list)
     for dep in legacy_metadata.dependencies:
         assert isinstance(dep, dict) or isinstance(dep, str)
 
