9b24bdb78966433359b8b3b2ace2172820b2d10e
James Tanner <tanner.jc@gmail.com>
2024-01-22 14:43:35 -0500

    Fix lint errors.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- tests/unit/test_loader_doc_string.py
+++ tests/unit/test_loader_doc_string.py
@@ -88,10 +88,12 @@ def test_run_ansible_doc_exception(mocked_popen, doc_string_loader):
     assert not res
 
 
-@mock.patch('galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES', ['module'])
+@mock.patch("galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES", ["module"])
 @mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc_list", return_value={})
 @mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc", return_value={})
-def test_ansible_doc_no_output(mocked_run_ansible_doc_list, mocked_run_ansible_doc, doc_string_loader):
+def test_ansible_doc_no_output(
+    mocked_run_ansible_doc_list, mocked_run_ansible_doc, doc_string_loader
+):
     assert doc_string_loader.load() == {}
 
 
@@ -362,10 +364,12 @@ def test_transform_doc_strings_nested_suboptions(doc_string_loader):
     ]
 
 
-@mock.patch('galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES', ['module'])
-@mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc_list", return_value={'my_module': {}})
+@mock.patch("galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES", ["module"])
+@mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc_list", return_value={"my_module": {}})
 @mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc", return_value=ANSIBLE_DOC_OUTPUT)
-def test_load_function(mocked_run_ansible_doc_list, mocked_run_ansible_doc, doc_string_loader, tmpdir):
+def test_load_function(
+    mocked_run_ansible_doc_list, mocked_run_ansible_doc, doc_string_loader, tmpdir
+):
 
     doc_string_loader.path = str(tmpdir)
     tmpdir.mkdir("plugins").mkdir("modules").join("my_module.py").write("")
@@ -390,8 +394,10 @@ def test_load_function(mocked_run_ansible_doc_list, mocked_run_ansible_doc, doc_
     }
 
 
-@mock.patch('galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES', ['inventory'])
-@mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc_list", return_value={'my_plugin': {}})
+@mock.patch(
+    "galaxy_importer.loaders.doc_string.constants.ANSIBLE_DOC_SUPPORTED_TYPES", ["inventory"]
+)
+@mock.patch.object(loaders.DocStringLoader, "_run_ansible_doc_list", return_value={"my_plugin": {}})
 @mock.patch("galaxy_importer.loaders.doc_string.Popen")
 def test_load_ansible_doc_error(mocked_popen, mocked_doc_list, doc_string_loader, tmpdir):
     mocked_popen.return_value.communicate.return_value = (
