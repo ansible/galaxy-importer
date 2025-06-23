5cd0f5c6098dadb03567bee8f80d53ee55505d0d
jerabekjiri <jerabekjirka@email.cz>
2025-06-23 11:56:05 +0200

    add pattern extraction

--- galaxy_importer/constants.py
+++ galaxy_importer/constants.py
@@ -55,6 +55,16 @@ ALLOWED_EXTENSION_DIRS = [
     EDA_EVENT_FILTER_NAME,
 ]
 
+# Ansible Patterns extension
+META_PATTERN_FILENAME = 'pattern.json'
+
+# Content-Types
+PATTERNS_NAME = "patterns"
+PATTERNS_EXECUTION_ENVIRONMENTS_NAME = "patterns/execution-environments"
+PATTERNS_META_NAME = 'patterns/meta'
+PATTERNS_PLAYBOOKS_NAME = 'patterns/playbooks'
+PATTERNS_TEMPLATES_NAME = 'patterns/templates'
+
 # Match github's allowable usernames, but also include underscores
 # because of old-galaxy's preference for replacing hyphens with underscores
 # in namespace names. There are quite a few legacy namespaces with
@@ -81,6 +91,7 @@ class ContentCategory(str, enum.Enum):
     PLUGIN = "plugin"
     PLAYBOOK = "playbook"
     EXTENSION = "extension"
+    PATTERN_EXTENSION = "pattern_extension"
 
 
 class ContentType(str, enum.Enum):
@@ -108,6 +119,12 @@ class ContentType(str, enum.Enum):
     EDA_EVENT_SOURCE = EDA_EVENT_SOURCE_NAME
     EDA_EVENT_FILTER = EDA_EVENT_FILTER_NAME
 
+    PATTERNS = PATTERNS_NAME
+    PATTERNS_EXECUTION_ENVIRONMENTS = PATTERNS_EXECUTION_ENVIRONMENTS_NAME
+    PATTERNS_META = PATTERNS_META_NAME
+    PATTERNS_PLAYBOOKS = PATTERNS_PLAYBOOKS_NAME
+    PATTERNS_TEMPLATES = PATTERNS_TEMPLATES_NAME
+
     @property
     def category(self):
         return {
@@ -134,4 +151,10 @@ class ContentType(str, enum.Enum):
             ContentType.VARS_PLUGIN: ContentCategory.PLUGIN,
             ContentType.EDA_EVENT_SOURCE: ContentCategory.EXTENSION,
             ContentType.EDA_EVENT_FILTER: ContentCategory.EXTENSION,
+
+            ContentType.PATTERNS: ContentCategory.PATTERN_EXTENSION,
+            ContentType.PATTERNS_EXECUTION_ENVIRONMENTS: ContentCategory.PATTERN_EXTENSION,
+            ContentType.PATTERNS_META: ContentCategory.PATTERN_EXTENSION,
+            ContentType.PATTERNS_PLAYBOOKS: ContentCategory.PATTERN_EXTENSION,
+            ContentType.PATTERNS_TEMPLATES: ContentCategory.PATTERN_EXTENSION,
         }.get(self)
--- galaxy_importer/finder.py
+++ galaxy_importer/finder.py
@@ -23,7 +23,8 @@ from typing import NamedTuple
 import attr
 
 from galaxy_importer import constants
-from galaxy_importer.file_parser import ExtensionsFileParser
+from galaxy_importer.file_parser import ExtensionsFileParser, PatternsParser
+from galaxy_importer.exceptions import ContentFindError
 
 default_logger = logging.getLogger(__name__)
 
@@ -33,8 +34,124 @@ class Result(NamedTuple):
     path: str
 
 
+class PatternFindError(ContentFindError):
+    def __init__(self, message="Default overwrite error occurred"):
+        self.message = message
+        super().__init__(self.message)
+
+
 ROLE_SUBDIRS = ["tasks", "vars", "handlers", "meta"]
 
+@attr.s(slots=True)
+class PatternsFinder:
+    path = attr.ib()
+    log = attr.ib()
+
+    def set_result(self, content_dir, content_type, file):
+        file_path = os.path.join(content_dir, file)
+        rel_path = self.get_rel_path(file_path)
+        yield Result(content_type, rel_path)
+
+    def set_results(self, content_dir, content_type, files):
+        for file in files:
+            yield from self.set_result(content_dir, content_type, file)
+
+    def get_rel_path(self, content_dir):
+        return os.path.relpath(content_dir, self.path)
+
+    def find_readme(self, content_dir):
+        readme = self._find_file(content_dir, "readme", [".md"])
+        yield from self.set_result(content_dir, constants.ContentType.PATTERNS, readme)
+
+    def find_setup(self, content_dir):
+        setup = self._find_file(content_dir, "setup")
+        yield from self.set_result(content_dir, constants.ContentType.PATTERNS, setup)
+
+    def find_meta_pattern(self, content_dir):
+        pattern = self._find_file(os.path.join(content_dir, "meta"), "pattern", allowed_extensions=[".json"])
+        yield from self.set_result(content_dir, constants.ContentType.PATTERNS_META, os.path.join("meta", pattern))
+
+    def find_execution_environment(self, content_dir):
+        # this directory is optional
+        # if present, it should contain exactly one yaml file
+        ee_dir = os.path.join(content_dir, "execution_environments")
+        if os.path.exists(ee_dir):
+            ee_files = self._map_dir(ee_dir)
+            if len(ee_files) != 1:
+                rel_path = self.get_rel_path(ee_dir)
+                raise ContentFindError(f"{rel_path} directory must contain exactly one execution environment file")
+            else:
+                yield from self.set_result(content_dir, constants.ContentType.PATTERNS_EXECUTION_ENVIRONMENTS, ee_files[0])
+        else:
+            ee_path = self.get_rel_path(ee_dir)
+            self.log.info(f"{ee_path} not found, skipping") # TODO: make this fnc
+
+    def find_playbooks(self, content_dir):
+        playbooks_dir = os.path.join(content_dir, "playbooks")
+
+        if not os.path.exists(playbooks_dir):
+            rel_path = self.get_rel_path(content_dir)
+            raise ContentFindError(f"{rel_path} must contain playbooks directory")
+        
+        playbooks = self._map_dir(playbooks_dir)
+        if len(playbooks) < 1:
+            rel_path = self.get_rel_path(playbooks_dir)
+            raise ContentFindError(f"{rel_path} must containt atleast one playbook")
+
+        yield from self.set_results(playbooks_dir, constants.ContentType.PATTERNS_PLAYBOOKS, playbooks)
+
+    def find_templates(self, content_dir):
+        templates_dir = os.path.join(content_dir, "templates")
+        if not os.path.exists(templates_dir):
+            rel_path = self.get_rel_path(templates_dir)
+            self.log.info(f"{rel_path} not found, skipping") # TODO: make this fnc
+        else:
+            templates = self._map_dir(templates_dir)
+            yield from self.set_results(templates_dir, constants.ContentType.PATTERNS_TEMPLATES, templates)
+
+    def _find_file(self, content_dir, expected_filename, allowed_extensions = [".yml", ".yaml"], required = True):
+        rel_path = self.get_rel_path(os.path.join(content_dir, expected_filename))
+
+        req_file_exc_msg = f"{rel_path}({'/'.join(allowed_extensions)}) not found"
+
+        if not os.path.exists(content_dir) and required:
+            raise ContentFindError(req_file_exc_msg)
+        else:
+            for content in os.listdir(content_dir):
+                if os.path.isfile(os.path.join(content_dir, content)):
+                    file, extension = os.path.splitext(content)
+                    if extension and extension.lower() in allowed_extensions and expected_filename == file.lower():
+                        return content
+
+        if required:
+            raise ContentFindError(req_file_exc_msg)
+
+    def _map_dir(self, content_dir):
+        dirs = []
+        for dirpath, _, filenames in os.walk(content_dir):
+            for filename in filenames:
+                dirs.append(os.path.join(dirpath, filename))
+
+        return dirs
+
+    def find_content(self, content_type, content_dir):
+        # readme.md
+        yield from self.find_readme(content_dir)
+
+        # setup.yml
+        yield from self.find_setup(content_dir)
+
+        # meta/pattern.json
+        yield from self.find_meta_pattern(content_dir)
+
+        # execution_environments/
+        yield from self.find_execution_environment(content_dir)
+
+        # playbooks/
+        yield from self.find_playbooks(content_dir)
+
+        # templates/
+        yield from self.find_templates(content_dir)
 
 class ContentFinder:
     """Searches for content in directories inside collection."""
@@ -49,7 +166,6 @@ class ContentFinder:
         self.path = path
         self.log = logger or default_logger
 
-        self.log.info("Finding content inside collection")
         contents = self._find_content()
 
         try:
@@ -125,6 +241,10 @@ class ContentFinder:
         for content_type, full_path in self._get_ext_types_and_path():
             yield content_type, full_path, self._find_plugins
 
+        patterns_finder = PatternsFinder(self.path, self.log)
+        for content_type, full_path in self._get_patterns_path():
+            yield content_type, full_path, patterns_finder.find_content
+
     def _get_ext_types_and_path(self):
         extension_dirs = ExtensionsFileParser(self.path).get_extension_dirs()
 
@@ -146,6 +266,19 @@ class ContentFinder:
 
         return content_types_and_dirs
 
+    def _get_patterns_path(self):
+        patterns_parser = PatternsParser(self.path)
+        patterns_dirs = patterns_parser.get_dirs()
+
+        if not patterns_dirs:
+            return []
+
+        content_types_and_dirs = [
+            (constants.ContentType.PATTERNS,
+            os.path.join(patterns_parser.relative_path, dir)) for dir in patterns_dirs
+        ]
+
+        return content_types_and_dirs
 
 @attr.s
 class FileWalker:
